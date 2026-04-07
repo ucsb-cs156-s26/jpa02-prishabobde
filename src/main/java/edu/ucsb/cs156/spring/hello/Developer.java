3f6599454f216717ef211563082fe521dbcfa1ab
Prisha Bobde <prishabobde@ucsb.edu>
2026-04-07 13:31:58 -0700

    full coverage for developer.java

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -16,7 +16,6 @@ public class Developer {
      */
 
     public static String getName() {
-        // TODO: Change this to your name
         // You may use just the name that is used on <https://bit.ly/cs156-s26-teams>
         // i.e. your first name, or your first and initial of last name
 
@@ -29,8 +28,7 @@ public class Developer {
      */
 
     public static String getGithubId() {
-        // TODO: Change this to your github id
-        return "cgaucho";
+        return "prishabobde";
     }
 
     /**
@@ -39,14 +37,13 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("s26-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s26-06");
+        team.addMember("Ethan");
+        team.addMember("Roy");
+        team.addMember("Dongyi");
+        team.addMember("Chaewon");
+        team.addMember("Aiden");
+        team.addMember("Prisha");
         return team;
     }
 }
--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -29,6 +29,28 @@ public class DeveloperTest {
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("prishabobde", Developer.getGithubId());
+    }
+
+    @Test
+    public void getTeam_returns_team_with_correct_name() {
+        Team  t = Developer.getTeam();
+        assertEquals("s26-06", t.getName());
+    }
+
+    @Test
+    public void getTeam_returns_team_with_correct_members() {
+        Team  t = Developer.getTeam();
+        assertTrue(t.getMembers().contains("Ethan"),"Team should contain Ethan");
+        assertTrue(t.getMembers().contains("Roy"),"Team should contain Roy");
+        assertTrue(t.getMembers().contains("Dongyi"),"Team should contain Dongyi");
+        assertTrue(t.getMembers().contains("Aiden"),"Team should contain Aiden");
+        assertTrue(t.getMembers().contains("Chaewon"),"Team should contain Chaewon");
+        assertTrue(t.getMembers().contains("Prisha"),"Team should contain Prisha");
+
+    }
 
 
 }
